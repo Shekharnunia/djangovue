3147818453618caa2b832dc16a4bccca9986e654
Shekhar <shekharnunia@gmail.com>
2019-07-05 16:38:27 +0530

    viewset created for blog model

---
+++ src/blog/viewsets.py
@@ -0,0 +1,7 @@
+from rest_framework import viewsets
+from .models import Blog
+from .serializer import BlogSerializer
+
+class BlogViewSet(viewsets.ModelViewSet):
+    queryset = Blog.objects.all()
+    serializer_class = BlogSerializer
