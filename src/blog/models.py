b1eacd4f87b3d48e42a459006b752bee72ca5458
Shekhar <shekharnunia@gmail.com>
2019-07-05 16:26:21 +0530

    blog model created in models.py file and version correction done in starter file

--- src/blog/models.py
+++ src/blog/models.py
@@ -1,3 +1,21 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class Blog(models.Model):
+    """This class will going to contain the blog database table data."""
+
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    title = models.CharField(max_length=50)
+    content = models.TextField()
+    timestamp = models.DateTimeField(auto_now=False, auto_now_add=False)
+
+    class Meta:
+        """Meta definition for Blog."""
+
+        verbose_name = 'Blog'
+        verbose_name_plural = 'Blogs'
+
+    def __str__(self):
+        """Unicode representation of Blog."""
+        return self.title
