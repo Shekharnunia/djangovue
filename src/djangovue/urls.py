94937d81ba33c53f37936bc33b1dffb8e0ccec4b
Shekhar <shekharnunia@gmail.com>
2019-07-05 16:57:35 +0530

    a path added to show blogs using html page

--- src/djangovue/urls.py
+++ src/djangovue/urls.py
@@ -1,9 +1,12 @@
 from django.contrib import admin
 from django.urls import path, include
+from django.views.generic import TemplateView
+
 
 from blog.routers import router
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include(router.urls)),
+    path('blog/', TemplateView.as_view(template_name='blog/blog.html')),
 ]
