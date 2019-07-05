3cd84d4fa437716b34c4c64545ac7ef5da7fcb95
Shekhar <shekharnunia@gmail.com>
2019-07-05 22:26:12 +0530

    css file added to show spinner and static root added to serve static files

--- src/djangovue/urls.py
+++ src/djangovue/urls.py
@@ -1,6 +1,9 @@
+from django.conf import settings
+from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path, include
 from django.views.generic import TemplateView
+
 
 
 from blog.routers import router
@@ -9,4 +12,4 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include(router.urls)),
     path('blog/', TemplateView.as_view(template_name='blog/blog.html')),
-]
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
